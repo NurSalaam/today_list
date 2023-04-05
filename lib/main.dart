5863225f68ad104fe23d93b41d3a5ba4d33ac58a
Nur Mogammad Salaam <36481471+NurSalaam@users.noreply.github.com>
2023-04-05 16:24:29 -0400

    implemented todaylistmanager

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:today_list/src/app.dart';
+import 'package:today_list/src/features/today_list/application/today_list_manager.dart';
 
 void main() {
+  WidgetsFlutterBinding
+      .ensureInitialized(); // Ensure the binding is initialized
+  TodayListManager();
   runApp(const MyApp());
 }
 
--- lib/src/features/today_list/data/today_item_repository.dart
+++ lib/src/features/today_list/data/today_item_repository.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:shared_preferences/shared_preferences.dart';
 import '../domain/today_item.dart';
 
@@ -12,9 +11,14 @@ class TodayItemRepository {
   TodayItemRepository._internal() {
     _initializeItemsController();
   }
+
+  Future<SharedPreferences> _getSharedPreferences() async {
+    return await SharedPreferences.getInstance();
+  }
+
   // fetchAllTodayItems
   Future<List<TodayItem>> fetchAllTodayItems() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
+    SharedPreferences prefs = await _getSharedPreferences();
     List<String>? itemsJson = prefs.getStringList(kTodayItemKey);
 
     if (itemsJson == null) {
@@ -24,16 +28,18 @@ class TodayItemRepository {
     return itemsJson.map((itemJson) => TodayItem.fromJson(itemJson)).toList();
   }
 
+  Future<void> _saveItems(List<TodayItem> items) async {
+    SharedPreferences prefs = await _getSharedPreferences();
+    List<String> itemsJson = items.map((item) => item.toJson()).toList();
+    await prefs.setStringList(kTodayItemKey, itemsJson);
+    _updateItemsController();
+  }
+
   // addTodayItem
   Future<void> addTodayItem(TodayItem item) async {
     List<TodayItem> allItems = await fetchAllTodayItems();
     allItems.add(item);
-
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    List<String> itemsJson = allItems.map((item) => item.toJson()).toList();
-    await prefs.setStringList(kTodayItemKey, itemsJson);
-    // * STREEEEEAAAAMMMSSS
-    _updateItemsController();
+    await _saveItems(allItems);
   }
 
   // editTodayItem
@@ -43,19 +49,11 @@ class TodayItemRepository {
 
     if (itemIndex != -1) {
       allItems[itemIndex] = updatedItem;
-
-      SharedPreferences prefs = await SharedPreferences.getInstance();
-      List<String> itemsJson = allItems.map((item) => item.toJson()).toList();
-      await prefs.setStringList(kTodayItemKey, itemsJson);
-      // * STREEEEEAAAAMMMSSS
-      _updateItemsController();
+      await _saveItems(allItems);
     } else {
       throw Exception('Item not found');
     }
   }
-
-  //* STREEEEEAAAAAMMMMMSSS
-  // today_item_repository.dart
 
   // StreamController
   final StreamController<List<TodayItem>> _itemsController =
@@ -64,30 +62,26 @@ class TodayItemRepository {
 
   Future<void> _initializeItemsController() async {
     List<TodayItem> allItems = await fetchAllTodayItems();
-    allItems.sort((a, b) => a.id.compareTo(b.id)); // Sort items by id
+    allItems.sort(
+        (a, b) => a.dateCreated.compareTo(b.dateCreated)); // Sort items by id
     _itemsController.add(allItems);
   }
 
   Future<void> _updateItemsController() async {
     List<TodayItem> allItems = await fetchAllTodayItems();
-    allItems.sort((a, b) => a.id.compareTo(b.id)); // Sort items by id
+    allItems.sort(
+        (a, b) => a.dateCreated.compareTo(b.dateCreated)); // Sort items by id
     _itemsController.add(allItems);
   }
 
-  Stream<List<TodayItem>> streamAllTodayItems() async* {
-    while (true) {
-      await Future.delayed(const Duration(
-          seconds: 1)); // Adjust the duration according to your needs
-      yield await fetchAllTodayItems();
-    }
-  }
-
   Future<void> clearAllItems() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
+    SharedPreferences prefs = await _getSharedPreferences();
     await prefs
         .setStringList(kTodayItemKey, []); // Set an empty list to kTodayItemKey
-
-    // Clear the repository
     _itemsController.add([]);
+  }
+
+  void dispose() {
+    _itemsController.close();
   }
 }
