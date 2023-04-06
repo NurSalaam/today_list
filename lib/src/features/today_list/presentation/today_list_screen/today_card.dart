47b6bf33831313f5256971bde31258f564ee53c6
Nur Mogammad Salaam <36481471+NurSalaam@users.noreply.github.com>
2023-04-06 11:28:28 -0400

    removed editing functionality

--- lib/src/features/today_list/presentation/today_list_screen/today_card.dart
+++ lib/src/features/today_list/presentation/today_list_screen/today_card.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:today_list/src/features/today_list/data/today_item_repository.dart';
 import 'package:today_list/src/features/today_list/domain/today_item.dart';
-import 'package:today_list/src/features/today_list/presentation/today_list_screen/edit_today_item_dialog.dart';
 
 class TodayCard extends StatelessWidget {
   final TodayItem todayItem;
@@ -17,48 +16,41 @@ class TodayCard extends StatelessWidget {
         : Colors.green.withOpacity(0.1);
     Color checkboxColor = isDarkMode ? Colors.white : Colors.black;
     Color checkColor = isDarkMode ? Colors.black : Colors.white;
-    return GestureDetector(
-      onTap: () {
-        if (!todayItem.isCompleted) {
-          showEditTodayItemDialog(context, todayItem);
-        }
-      },
-      child: Container(
-        decoration: todayItem.isCompleted
-            ? BoxDecoration(
-                color: greenColor,
-                borderRadius: BorderRadius.circular(8),
-              )
-            : null,
-        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-        child: Row(
-          children: [
-            Checkbox(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(4),
-              ),
-              checkColor: checkColor,
-              fillColor: MaterialStateProperty.resolveWith((states) {
-                if (states.contains(MaterialState.selected)) {
-                  return checkboxColor;
-                }
-                return isDarkMode ? Colors.white54 : Colors.black54;
-              }),
-              value: todayItem.isCompleted,
-              onChanged: (bool? value) {
-                bool isComplete = !todayItem.isCompleted;
-                TodayItem updatedItem =
-                    todayItem.copyWith(isCompleted: isComplete);
-                TodayItemRepository().editTodayItem(updatedItem);
-              },
+    return Container(
+      decoration: todayItem.isCompleted
+          ? BoxDecoration(
+              color: greenColor,
+              borderRadius: BorderRadius.circular(8),
+            )
+          : null,
+      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      child: Row(
+        children: [
+          Checkbox(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(4),
             ),
-            Expanded(
-              child: Text(
-                todayItem.text,
-              ),
+            checkColor: checkColor,
+            fillColor: MaterialStateProperty.resolveWith((states) {
+              if (states.contains(MaterialState.selected)) {
+                return checkboxColor;
+              }
+              return isDarkMode ? Colors.white54 : Colors.black54;
+            }),
+            value: todayItem.isCompleted,
+            onChanged: (bool? value) {
+              bool isComplete = !todayItem.isCompleted;
+              TodayItem updatedItem =
+                  todayItem.copyWith(isCompleted: isComplete);
+              TodayItemRepository().editTodayItem(updatedItem);
+            },
+          ),
+          Expanded(
+            child: Text(
+              todayItem.text,
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
